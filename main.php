2d675f7e0e20258d87b55fb1685781c244dbdedc
sammanthp007 <samman.thapa@bison.howard.edu>
2017-01-11 03:25:24 -0500

    make runnable tippy

--- main.php
+++ main.php
@@ -10,17 +10,47 @@
         <h2>
             Calculate your tip
         </h2>
-        <form action="main.php" action="post">
-            <label for="billInput">Bill:  $</label>
-            <input type="text">
-            <br>
-            <label for="tipPercentage">Tip Percentage</label>
-            <input type="radio" name="tipPercentage" value="male" checked> 10%
-            <input type="radio" name="tipPercentage" value="female"> 15%
-            <input type="radio" name="tipPercentage" value="other"> 20%<br>
-            <input type="submit" value="Submit">
-        </form>
+            <div>
+                <form action="main.php" method="post">
+                    <label for="billInput">Bill:  $</label>
+                    <input type="text" name="billInput" value="<?php echo isset($_POST['billInput']) ? $_POST['billInput'] : NULL; ?>" />
+                    <br>
+                    
+                    <label for="tipPercentage">Tip Percentage</label>
+                    <?php
+                        $tips = [10, 15, 20];
+                        for ($i=0; $i < 3; $i++) {
+                    ?>
+                    <input type="radio" name="tipPercentage" value="<?php echo $tips [$i]; ?>"> <?php echo $tips[$i]."%"; ?>
+                    <?php
+                        }
+                    ?>
+                    <br>
+                    <input type="submit" value="Submit" name="submit"/>
+                </form>
+            </div>
 
+            <div>
+                <?php
+                    $input_total = $_POST["billInput"];
+                    $input_tip = $_POST["tipPercentage"];
+                    if (isset($_POST["submit"])) {
+                        echo "<hr>";
+                        if ($input_total && $input_tip) {
+                            if (is_numeric($input_total) && floatval($input_tip) >= 0) {
+                                $total_pay = floatval($input_total);
+                                $tip_total = (floatval($input_tip) / 100) * $total_pay;
+                                $total_pay = $total_pay + $tip_total;
+
+                                echo "Tip:      $";
+                                echo number_format($tip_total, 2)."<br>";
+
+                                echo "Total:    $";
+                                echo number_format($total_pay, 2)."<br>";
+                            }
+                        }
+                    }
+                ?>
+            </div>
     </body>
 </html>
-
